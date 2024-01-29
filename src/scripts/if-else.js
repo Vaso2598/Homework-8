7c5c8f9eaceff2490964b652ca499eb4c8131d48
Vasili <va.chanishvili@gmail.com>
2024-01-29 22:43:59 +0400

    finished script file

--- src/scripts/if-else.js
+++ src/scripts/if-else.js
@@ -1,11 +1,22 @@
-var balance = prompt("შეიყვანეთ ბალანსი", 0)
+var balance = prompt("შეიყვანეთ ბალანსი", 0);
 
-var percent = prompt("შეიყვანეთ დარიცხული პროცენტი", 10)
+var percent = prompt("შეიყვანეთ დარიცხული პროცენტი", 10);
 
-var output = parseInt(balance)
+var output = parseInt(balance);
 
-var percentInt = parseInt(percent)
+var percentInt = parseInt(percent);
 
-output = output + output * percentInt / 100
+output = output + output * percentInt / 100;
 
-alert("პროცენტის დარიცხვის შემდეგ თანხა იქნება " + output)
+if(output > 1500){
+    alert("პროცენტის დარიცხვის შემდეგ თანხა იქნება " + output);
+    alert("თქვენ გაქვთ საშუალოზე მეტი თანხა")
+}
+else if(output > 0){
+    alert("პროცენტის დარიცხვის შემდეგ თანხა იქნება " + output);
+    alert("პთქვენ გაქვთ საშუალოზე ნაკლები თანხა");
+}
+else{
+    alert("პროცენტის დარიცხვის შემდეგ თანხა იქნება " + output);
+    alert("პთქვენ ანგარიშზე თანხა არ არის");
+}
